a4d66f615c0640dba07aeb4af4402d77eec20965
roachseb <sebastien.r.r@hotmail.com>
2022-06-01 20:33:01 +0200

    adding json

--- src/main.py
+++ src/main.py
@@ -1,3 +1,4 @@
+from pandas import DataFrame
 from pytrends.request import TrendReq
 from typing import Union
 from fastapi import FastAPI
@@ -7,3 +8,8 @@ app = FastAPI()
 @app.get("/")
 async def read_root():
     return {"This": "Update"}
+
+@app.get("/regions/{_region}")
+async def get_trend_by_region(region: int):
+    df:DataFrame= TrendReq.interest_by_region(resolution="Python")
+    return df.to_json()

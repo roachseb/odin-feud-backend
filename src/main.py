53d9c45aaf81044a372fec7fb4154a8ddcf6a429
roachseb <sebastien.r.r@hotmail.com>
2022-06-02 15:10:39 +0200

    my changes

--- src/main.py
+++ src/main.py
@@ -1,15 +1,33 @@
-from pandas import DataFrame
+from pandas import DataFrame, concat
 from pytrends.request import TrendReq
-from typing import Union
+from typing import List, Union
 from fastapi import FastAPI
 
 app = FastAPI()
+pytrends = TrendReq()
+
+
+def get_suggestions(word:str) -> List[str]:
+    return [ v for dic in pytrends.suggestions(word) for k,v in dic.items() if k == "title"]
 
 @app.get("/")
 async def read_root():
     return {"This": "Update"}
 
-@app.get("/regions/{_region}")
-async def get_trend_by_region(region: int):
-    df:DataFrame= TrendReq.interest_by_region(resolution="Python")
-    return df.to_json()
+
+@app.get("/regions/{word}")
+async def get_trend_by_region(word: str, timeframe:str="today 1-m", suggestion: bool=False):
+    if suggestion:
+        word = get_suggestions(word)
+    pytrends.build_payload(word,timeframe=timeframe)
+    res_dict = pytrends.interest_by_region().mean(axis=1).nlargest(6).to_dict()
+    return [{"text": k, "money": v/100*50} for k,v in res_dict.items()]
+
+
+@app.get("/interest/{word}")
+async def get_trend(word: str, timeframe:str="today 1-m", suggestion: bool=False):
+    if suggestion:
+        word = get_suggestions(word)
+    pytrends.build_payload(word,timeframe=timeframe)
+    res_dict = pytrends.interest_over_time()[pytrends.interest_over_time()['isPartial']==False].drop(["isPartial"],axis=1).mean(axis=0).to_dict()
+    return [{"text": k, "money": v/100*50} for k,v in res_dict.items()]
